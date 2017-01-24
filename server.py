969a6e211b5753f7e338a80ca3a54a201349a21d
IlyaDjurin <lev2048@gmail.com>
2017-01-24 16:23:29 +0300

    продолжаем с бустстрапом

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ my_flask_app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@my_flask_app.route('/inf/')
+def info():
+	return render_template('info_gl_str.html')	
+
 if 	__name__ == "__main__":
 	my_flask_app.run(debug=True)
