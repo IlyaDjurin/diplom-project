6d64bd91515808506320ece798064fec37a6c16d
IlyaDjurin <lev2048@gmail.com>
2017-01-24 21:27:11 +0300

    добавил пароль и меню авторизации

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request
 
 my_flask_app = Flask(__name__)
 
@@ -9,6 +9,10 @@ def index():
 @my_flask_app.route('/inf/')
 def info():
 	return render_template('info_gl_str.html')	
+
+@my_flask_app.route('/login/',methods = ['POST'])
+def login():
+		return render_template('login.html',email=request.form.get("email"), password=request.form.get("passwd"))	
 
 if 	__name__ == "__main__":
 	my_flask_app.run(debug=True)
