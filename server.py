23c6d2e8961a2a68080adef5ec52dd488b2c835a
IlyaDjurin <lev2048@gmail.com>
2017-01-28 21:10:52 +0300

    ..

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask , render_template, request
+from db_magazina import Kategor, Tovar
+
 
 my_flask_app = Flask(__name__)
 
@@ -17,6 +19,14 @@ def log1():
 @my_flask_app.route('/login/',methods = ['POST'])
 def login():
 		return render_template('login.html',email=request.form.get("email"), password=request.form.get("passwd"))	
+
+@my_flask_app.route('/category/<int:cat_id>')
+def category(cat_id):
+	t = Tovar
+	my_tovar = t.query.filter(Tovar.kategory_id == cat_id).all()
+	for m in my_tovar:
+		print(m.tovar_name)
+	return "ok"
 
 if 	__name__ == "__main__":
 	my_flask_app.run(debug=True)
