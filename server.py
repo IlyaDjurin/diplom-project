554a74a3a78b2cd3680a23555dd30163b27dd7c8
IlyaDjurin <lev2048@gmail.com>
2017-02-03 17:54:04 +0300

    добавлена страница со смартфонами

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask , render_template, request
-from db_magazina import Kategor, Tovar
+from db_magazina import Kategor, Tovar, Tovar_photo
 
 
 my_flask_app = Flask(__name__)
@@ -20,13 +20,19 @@ def log1():
 def login():
 		return render_template('login.html',email=request.form.get("email"), password=request.form.get("passwd"))	
 
-@my_flask_app.route('/category/<int:cat_id>')
-def category(cat_id):
-	t = Tovar
-	my_tovar = t.query.filter(Tovar.kategory_id == cat_id).all()
-	for m in my_tovar:
-		print(m.tovar_name)
-	return "ok"
+@my_flask_app.route('/smart/', methods = ['get'])
+def category():
+	t = Tovar()
+	t1=Tovar_photo()
+	c=t1.query.get(2).tovarphoto_image
+	print(c)
+	a=t.query.get(5).tovar_name
+	b=t.query.get(5).tovar_info
+		
+	return render_template('smartfons.html', baza = a , baza1 = b , baza2 = c , baza3 =  t1.query.get(3).tovarphoto_image, \
+		 baza4 = t1.query.get(3).tovari.tovar_info ,  baza5 = t1.query.get(3).tovari.tovar_name,\
+		 baza6 = t1.query.get(4).tovarphoto_image , baza7 = t1.query.get(4).tovari.tovar_info ,baza8 = t1.query.get(4).tovari.tovar_name,\
+		 baza9 = t1.query.get(5).tovarphoto_image , baza10 = t1.query.get(5).tovari.tovar_info ,baza11 = t1.query.get(5).tovari.tovar_name)
 
 if 	__name__ == "__main__":
 	my_flask_app.run(debug=True)
